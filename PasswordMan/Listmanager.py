5d9265ee7276f74e9255458399e4661001b9f6e2
Marius <marign16>
2018-09-26 19:45:49 +0200

    Updated password discovery with counters

--- PasswordMan/Listmanager.py
+++ PasswordMan/Listmanager.py
@@ -1,8 +1,8 @@
 myfile = open("passwordlist.txt", "r")
 
 file = myfile.read().split(";")
-print(file)
 
+counter = 0
 filteredList = {}
 for entity in file:
     splitEntity = entity.split(":")
@@ -12,7 +12,16 @@ for entity in file:
         filteredList[key] = [value]
     else:
         filteredList[key].append(value)
+    counter += 1
 
 for key in filteredList:
+    buriedArray = filteredList.get(key)
     print("The passwords for user " + key + " are: ")
     print(filteredList[key])
+    print(key + " therefore has " + str(len(buriedArray)) + " password(s)")
+    print()
+
+print("There are " + str(counter) + " passwords in total")
+
+
+
