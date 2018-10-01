20d4d8cd225aba9b94ff7f83e5fca07ced960220
emilr15 <emilr15@uia.no>
2018-10-01 23:17:37 +0200

    Fjernet litt rusk

--- Object Oriented Python.py
+++ Object Oriented Python.py
@@ -1,5 +1,5 @@
-from platform   import system as system_name  # Returns the system/OS name
-from subprocess import call   as system_call  # Execute a shell command
+from platform import system as system_name
+from subprocess import call as system_call
 
 class PingIP:
 
